73b8c43dc255340fe2fddf282c4be8ec470ab142
matheus <mbarbosa-silva@myseneca.ca>
2019-07-20 17:23:41 -0400

    Sign up user working

--- app/src/main/java/com/bookswap/ui/user/CreateAccountActivity.java
+++ app/src/main/java/com/bookswap/ui/user/CreateAccountActivity.java
@@ -22,8 +22,10 @@ import com.bookswap.model.user.User;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -72,115 +74,48 @@ public class CreateAccountActivity extends AppCompatActivity {
         String username = editTextUsername.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
-        String confirm = editTextConfirm.getText().toString().trim();
 
-        //validate
-        /*if (username.isEmpty()) {
-            editTextUsername.setError("Username is required");
-            editTextUsername.requestFocus();
-            return;
-        }
-        if (email.isEmpty()) {
-            editTextEmail.setError("Email is required");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTextEmail.setError("Enter a valid email");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if (password.isEmpty()) {
-            editTextPassword.setError("Password is required");
-            editTextPassword.requestFocus();
-            return;
-        }
-        if (password.length() < 6) {
-            editTextPassword.setError("Minimum 6 characters");
-            editTextPassword.requestFocus();
-            return;
-        }
-        if (!password.equals(confirm)) {
-            editTextConfirm.setError("Passwords must match");
-            editTextConfirm.requestFocus();
-            return;
-        }*/
-        User user = new User();
-        user.setEmail(email);
-        user.setUsername(username);
-        user.setPassword(password);
+        HashMap<String,Object> user_ = new HashMap<>();
 
+        HashMap<String,String> hashRoles = new HashMap<>();
+        hashRoles.put("name","USER");
+        HashMap[] roles_ = {hashRoles};
 
-        String url = "http://myvmlab.senecacollege.ca:6510/bookswap-0.0.1/user/signup";
+        HashMap<String,String> campus_ = new HashMap<>();
+        campus_.put("name","name");
 
-       /* try{
-            URL myurl = new URL(url);
+        HashMap<String,String> address_ = new HashMap<>();
+        address_.put("addressLine1","addressLine1");
+        address_.put("addressLine2","addressLine2");
+        address_.put("addressLine3","addressLine3");
+        address_.put("city","city");
+        address_.put("province","province");
+        address_.put("country","country");
+        address_.put("postalCode","pose");
 
-            //set request method header
-            con.setRequestMethod("POST");
-
-            con = (HttpURLConnection) myurl.openConnection();
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setRequestProperty("Authorization", "Basic");
-
-            StringBuilder content;
-            try (BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream()))){
-                String line;
-                content = new StringBuilder();
-
-                while((line = in.readLine()) != null){
-                    content.append(line);
-                    content.append(System.lineSeparator());
-                }
-            }
-            // System.out.println(content.toString());
-
-            Toast.makeText(CreateAccountActivity.this, content.toString(), Toast.LENGTH_LONG).show();
-        } finally */
-
-            //user registration using api call
-            //Call<StdResponse> call = APIClient.getInstance().getUserService().signup(email,username,password);
-
-        Campus campus = new Campus();
-    	campus.setName("");
-        List<Role> roles = new ArrayList<>();
-    	roles.add(new Role(""));
-    	roles.add(new Role(""));
-
-    	List<Address> address = new ArrayList<>();
-    	address.add(new Address("", "", "", "", "",
-    					"",""));
-
-    	User user2 = new User("ryan", "marzec", "marzecreyan@gmail.com", "appMarss1", "ryan123",
-    			roles,campus,address);
+        user_.put("email",email);
+        user_.put("username",username);
+        user_.put("password",password);
+        user_.put("roles",roles_);
+        user_.put("campus",campus_);
+        user_.put("address",address_);
 
         try {
             APIClient api = new APIClient().getInstance();
-            Call<StdResponse> call = api.getUserService().signup(user2);
+            Call<StdResponse> call = api.getUserService().signup(user_);
+
             call.enqueue(new Callback<StdResponse>() {
                 @Override
                 public void onResponse(Call<StdResponse> call, Response<StdResponse> response){
-                   // onNetworkRequestFinished();
-                    /*if (response.isSuccessful()){
-                        int retStatus = response.code();
-                        //Toast.makeText(CreateAccountActivity.this, "success", Toast.LENGTH_LONG).show();
-                        Toast.makeText(CreateAccountActivity.this, Integer.toString(retStatus), Toast.LENGTH_LONG).show();
-                    } else {
-                        int retStatus = response.code();
-                        Toast.makeText(CreateAccountActivity.this, Integer.toString(retStatus), Toast.LENGTH_LONG).show();
-                        //Toast.makeText(CreateAccountActivity.this, "fail", Toast.LENGTH_LONG).show();
-                    }*/
-                    if(!response.isSuccessful()){
                         try{
                             int retStatus = response.code();
                             Toast.makeText(CreateAccountActivity.this, Integer.toString(retStatus), Toast.LENGTH_LONG).show();
                             String responseX ="";
                             responseX = response.errorBody().string();
                             Log.d("TEST1",responseX);
-                        }catch (IOException e){
+                        }catch (Exception e){
                             Log.e("Booking Presenter", "Exception");
                         }
-                    }
                 }
 
                 @Override
